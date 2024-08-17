fca8b173d2edb0320fbd8c00429e33e8371c3481
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-08-17 00:15:56 +0000

    mycommit

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Lucy " : variables.name + " "}${
+    variables.lastName == null ? "Boilett" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "Web Developer" : variables.role}</h2>
+          <h3>${variables.city == null ? "Miami" : variables.city}, ${
+    variables.country == null ? "USA" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>

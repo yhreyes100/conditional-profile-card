b43e62626025faaa3059454c010e8e9afb2e6ccc
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-08-24 01:07:48 +0000

    comm

--- src/js/app.js
+++ src/js/app.js
@@ -44,22 +44,22 @@ function render(variables = {}) {
             <li><a href="${
               variables.twitter == null
                 ? "https://twitter.com/4geeksacademy"
-                : variables.twitter
+                : "https://twitter.com/" + variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="${
               variables.github == null
                 ? "https://github.com/4geeksacademy"
-                : variables.github
+                : "https://github.com/" + variables.github
             }"><i class="fab fa-github"></i></a></li>
             <li><a href="${
               variables.linkedin == null
                 ? "https://linkedin.com/school/4geeksacademy"
-                : variables.linkedin
+                : "https://linkedin.com/school/" + variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="${
               variables.instagram == null
                 ? "https://instagram.com/4geeksacademy"
-                : variables.instagram
+                : "https://instagram.com/" + variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
